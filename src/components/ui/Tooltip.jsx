0440437aea082a2a08f4ff7ea017828149eab5b1
maxiseeh <maxymeeh@gmail.com>
2026-05-27 23:27:12 +0300

    restore: original source code

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -1,1 +1,24 @@
+import * as React from "react"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
+import { cn } from "@/lib/utils"
+
+const TooltipProvider = TooltipPrimitive.Provider
+
+const Tooltip = TooltipPrimitive.Root
+
+const TooltipTrigger = TooltipPrimitive.Trigger
+
+const TooltipContent = ({ className, ...props }) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      className={cn(
+        "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground",
+        className
+      )}
+      {...props}
+    />
+  </TooltipPrimitive.Portal>
+)
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
